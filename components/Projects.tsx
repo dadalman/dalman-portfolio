7c18597bb4e24e6e6058486edd868880d5204dd9
Denver Dalman <81907273+dadalman@users.noreply.github.com>
2025-04-19 02:30:26 +0800

    Updated project spread

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,36 +1,36 @@
 export default function Projects() {
   const projects = [
     {
+      title: "Enstack: AI-Powered Entrepreneurship",
+      description:
+        "As a former team member at Enstack, I played a role in designing and developing its core mobile and web applications. I focused on crafting a seamless, user-centric experience across platforms and integrating APIs.",
+      image: "/assets/images/enstack-banner.png",
+      tags: ["Figma", "Angular", "Mobile App", "Sass"],
+      link: "https://www.enstack.com/downloads",
+    },
+    {
+      title: "Enstack Magic",
+      description:
+        "Enstack Magic is a standalone AI-powered tool designed to assist entrepreneurs. I took the lead in front-end development, focusing on creating a smooth and responsive user interface.",
+      image: "/assets/images/enstack-magic-banner.png",
+      tags: ["Angular", "Mobile App", "Sass"],
+      link: "https://www.enstack.com/magic",
+    },
+    {
+      title: "Fanhub: Idol NFC Community",
+      description:
+        "Fanhub is a social app prototype I designed, where fans can collect, trade, and interact using NFC-powered idol cards and access exclusive fan perks. The concept brings physical fan experiences into the digital world.",
+      image: "/assets/images/fanhub-banner.png",
+      tags: ["Figma", "Hi-Fidelity", "Prototype"],
+      link: "https://www.figma.com/proto/66DGpp3oPOLSFLrEFTiWWS/Fanhub-Design?node-id=21-4900&starting-point-node-id=21%3A4900",
+    },
+    {
       title: "Be the NEXT: My Bias List",
       description:
         "A web app that lets users rank their favorite trainees from a survival show.",
       image: "/assets/images/btn-bias-list-banner.png",
       tags: ["next.js", "tailwindcss", "figma"],
       link: "https://btn-my-bias-list.vercel.app/",
-    },
-    {
-      title: "Enstack Magic",
-      description:
-        "Worked on the front-end of Enstack Magic, an AI-powered tool that helps entrepreneurs instantly generate product descriptions, promotional posts, and sales content.",
-      image: "/assets/images/enstack-magic-banner.png",
-      tags: ["angular", "mobile app", "sass"],
-      link: "https://www.enstack.com/magic",
-    },
-    {
-      title: "Fanhub: Idol NFC Community",
-      description:
-        "A social app prototype where fans can collect, trade, and interact using NFC-powered idol cards and exclusive fan perks.",
-      image: "/assets/images/fanhub-banner.png",
-      tags: ["figma", "hi-fidelity", "prototype"],
-      link: "https://www.figma.com/proto/66DGpp3oPOLSFLrEFTiWWS/Fanhub-Design?node-id=21-4900&starting-point-node-id=21%3A4900",
-    },
-    {
-      title: "OwnFlix",
-      description:
-        "A mobile movie app built with React Native by following a guided tutorial. Features include movie browsing and detail views.",
-      image: "/assets/images/mobile-movie-banner.png",
-      tags: ["react native", "expo", "mobile app"],
-      link: "https://github.com/dadalman/mobile-movie-app",
     },
     {
       title: "MemoRise",
@@ -41,13 +41,21 @@ export default function Projects() {
       link: "https://www.figma.com/proto/95QtUeX5RpYNsEE11iO0he/MemoRise?node-id=1-2&starting-point-node-id=1%3A2",
     },
     {
-      title: "LocaLink",
+      title: "OwnFlix",
       description:
-        "Connects locals to share knowledge and services—farmers, teachers, shopkeepers, and more.",
-      image: "/assets/images/localink-banner.png",
-      tags: ["figma", "hi-fidelity", "prototype"],
-      link: "https://www.figma.com/proto/t8MVZIjEyrwTQ3E8itcHoc/LocaLink?node-id=11-499&p=f&t=i6AMhAduz2DOrNgw-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=11%3A499",
+        "A mobile movie app built with React Native by following a guided tutorial. Features include movie browsing and detail views.",
+      image: "/assets/images/mobile-movie-banner.png",
+      tags: ["react native", "expo", "mobile app"],
+      link: "https://github.com/dadalman/mobile-movie-app",
     },
+    // {
+    //   title: "LocaLink",
+    //   description:
+    //     "Connects locals to share knowledge and services—farmers, teachers, shopkeepers, and more.",
+    //   image: "/assets/images/localink-banner.png",
+    //   tags: ["figma", "hi-fidelity", "prototype"],
+    //   link: "https://www.figma.com/proto/t8MVZIjEyrwTQ3E8itcHoc/LocaLink?node-id=11-499&p=f&t=i6AMhAduz2DOrNgw-0&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=11%3A499",
+    // },
 
     // Add more projects here if needed
   ];
